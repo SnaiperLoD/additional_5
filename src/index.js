f0fc78e002b53047b9faad4dc2730b1ec9c53cf5
SnaiperLoD <snaiperlod19@gmail.com>
2018-10-07 19:46:12 +0300

    20/20

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  return true;
+  let initial=str;
+  const size=bracketsConfig.length;
+  
+  for(let n=initial.length; n>=0; n--) 
+  {
+    for(let k=0; k<size; k++)
+    {
+      if(initial[n]==bracketsConfig[k][0])
+      {
+        initial=initial.replace(bracketsConfig[k].join(''), '');
+      }
+    }
+  }
+
+  return (initial === "");
 }
